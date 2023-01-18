2a0b8fb70fb2612eb7a7eeb94b6d7989213cdae1
Micah Sudweeks <micahsudweeks@gmail.com>
2023-01-18 14:15:20 -0700

    Updated login.cmd, package.json, and server.js to utilize the server we are creating instead of the one created by Bro Murdock

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require ("express");
+
+const app = express();
+
+const port = 3000;
+
+const bodyParser = require ('body-parser');
+
+app.use(bodyParser.json()); //This looks for incoming data
+
+app.get("/", (req, res)=> {
+    res.send("Hello Micah");
+});
+
+app.post('/login', (req, res) =>{
+    const loginUser = req.body.userName;
+    console.log('Login username: '+loginUser);
+    res.send('Hello '+loginUser);
+});
+
+app.listen(port, () =>{
+    console.log("Listening");
+});
